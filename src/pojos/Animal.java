3c172dfa7fe147183fb7f40577b8dff9ee5faa9b
Propietario <Propietario@DESKTOP-U9AVVA6>
2020-04-13 18:48:36 +0200

    pojos

--- src/pojos/Animal.java
+++ src/pojos/Animal.java
@@ -1,80 +1,38 @@
 package pojos;
 
-import java.io.Serializable;
 import java.sql.Date;
 
-public class Animal implements Serializable {
+public class Animal {
 	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -2133854860664818383L;
-
-
-	private Integer id_a;
-	private String name_a;
-	private Date dob_a;
-	private String specie_a;
-	private String colour;
+	private Integer id;
+	private String name;
 	private String country;
+	private String colour;
+	private String specie;
+	private Date dob;
 	
-	
-	//Contructors calling variables, one by default, one with id_a & one without it
-	public Animal () {
+	//Constructor
+	public Animal(Integer id, String name, String specie) {
 		super();
+		this.id = id;
+		this.name = name;
+		this.specie = specie;
 	}
 	
-	public Animal (Integer id_a, String name_a, Date dob_a, String specie_a, String colour, String country) {
-		super();
-		this.id_a= id_a;
-		this.name_a = name_a;
-		this.dob_a = dob_a;
-		this.specie_a =specie_a;
-		this.colour = colour;
-		this.country = country;
-		
+	
+	//Getters y setters
+	public Integer getId() {
+		return id;
 	}
 	
-	public Animal (String name_a, Date dob_a, String specie_a, String colour, String country) {
-		super();
-		this.name_a = name_a;
-		this.dob_a = dob_a;
-		this.specie_a =specie_a;
-		this.colour = colour;
-		this.country = country;
-		
+	public void setId(Integer id) {
+		this.id = id;
 	}
-	
-	//Gets and Sets of Animals
-	public Integer getId_a() {
-		return id_a;
+	public String getName() {
+		return name;
 	}
-	public void setId_a(Integer id_a) {
-		this.id_a = id_a;
-	}
-	public String getName_a() {
-		return name_a;
-	}
-	public void setName_a(String name_a) {
-		this.name_a = name_a;
-	}
-	public Date getDob_a() {
-		return dob_a;
-	}
-	public void setDob_a(Date dob_a) {
-		this.dob_a = dob_a;
-	}
-	public String getSpecie_a() {
-		return specie_a;
-	}
-	public void setSpecie_a(String specie_a) {
-		this.specie_a = specie_a;
-	}
-	public String getColour() {
-		return colour;
-	}
-	public void setColour(String colour) {
-		this.colour = colour;
+	public void setName(String name) {
+		this.name = name;
 	}
 	public String getCountry() {
 		return country;
@@ -82,39 +40,34 @@ public class Animal implements Serializable {
 	public void setCountry(String country) {
 		this.country = country;
 	}
+	public String getColour() {
+		return colour;
+	}
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+	public String getSpecie() {
+		return specie;
+	}
+	public void setSpecie(String specie) {
+		this.specie = specie;
+	}
+	public Date getDob() {
+		return dob;
+	}
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
 
-	//What the console shows if we call Animals
+
+	@Override
 	public String toString() {
-		return "Animal id= " +id_a +"/s"
-			+"Animal name= " +name_a +"/s"
-			+"Animal date of birth= "+dob_a +"/s"
-			+"Kind of animal= " +specie_a +"/s"
-			+"Colour of the animal= " +colour +"/s"
-			+"Country from which it comes= " +country;
+		return "Animal [id=" + id + ", name=" + name + ", country=" + country + ", colour=" + colour + ", specie="
+				+ specie + ", dob=" + dob + "]";
 	}
 	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id_a == null) ? 0 : id_a.hashCode());
-		return result;
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Animal other = (Animal) obj;
-		if (id_a == null) {
-			if (other.id_a != null)
-				return false;
-		} else if (!id_a.equals(other.id_a))
-			return false;
-		return true;
-	}
+	//tostring
 	
+	
+
 }
--- src/pojos/Chocolate.java
+++ src/pojos/Chocolate.java
@@ -1,145 +1,108 @@
 package pojos;
 
-import java.io.Serializable;
-
-public class Chocolate implements Serializable {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -2290544292024275090L;
-		//no tenemos ningun dato relacionado con dato 
-   // podriamos poner un dato que fuera la fecha de creacion de ese chocolate 
-	//Date: siempre import java.sql
-        private Integer id;
-        private String name;
-        private int cocoa;
-        private String type;
-        private String flavors;
-        private Float units;
-        private String shape;
-        
-        
-        public Chocolate(Integer id, String name, int cocoa, String type, String flavors, Float units,
-				String shape) {
-			super();
-			this.id = id;
-			this.name = name;
-			this.cocoa = cocoa;
-			this.type = type;
-			this.flavors = flavors;
-			this.units = units;
-			this.shape = shape;
-		}
-        
-        
-        
-        
-		public Chocolate(String name, int cocoa, String type, String flavors, Float units, String shape) {
-			super();
-			this.name = name;
-			this.cocoa = cocoa;
-			this.type = type;
-			this.flavors = flavors;
-			this.units = units;
-			this.shape = shape;
-		}
 
 
 
-
-		//CREACION DEL CONSTRUCTOR 
-		public Chocolate() {
-			super();
-		}
-		//GETTERS AND SETTERS
-
-		public Integer getId() {
-			return id;
-		}
-
-		public void setId(Integer id) {
-			this.id = id;
-		}
-
-		public String getName() {
-			return name;
-		}
-
-		public void setName(String name) {
-			this.name = name;
-		}
-
-		public int getCocoa() {
-			return cocoa;
-		}
-
-		public void setCocoa(int cocoa) {
-			this.cocoa = cocoa;
-		}
-
-		public String getType() {
-			return type;
-		}
-
-		public void setType(String type) {
-			this.type = type;
-		}
-
-		public String getFlavors() {
-			return flavors;
-		}
-
-		public void setFlavors(String flavors) {
-			this.flavors = flavors;
-		}
-
-		public Float getUnits() {
-			return units;
-		}
-
-		public void setUnits(Float units) {
-			this.units = units;
-		}
-
-		public String getShape() {
-			return shape;
-		}
-
-		public void setShape(String shape) {
-			this.shape = shape;
-		}
-		
-        //MÉTODO TOSTRING()
-		@Override
-		public String toString() {
-			return "Chocolate [id=" + id + ", name=" + name + ", cocoa=" + cocoa + ", type=" + type + ", flavors="
-					+ flavors + ", units=" + units + ", shape=" + shape + "]";
-		}
-
-		@Override
-		public int hashCode() {
-			final int prime = 31;
-			int result = 1;
-			result = prime * result + ((id == null) ? 0 : id.hashCode());
-			return result;
-		}
-
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
-				return true;
-			if (obj == null)
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			Chocolate other = (Chocolate) obj;
-			if (id == null) {
-				if (other.id != null)
-					return false;
-			} else if (!id.equals(other.id))
-				return false;
+public class Chocolate {
+	
+	private Integer id;
+	private String name;
+	private String type;
+	private Float cocoa_perc;
+	private String flavour;
+	private Integer unit;
+	private String shape;
+	
+	
+	//CONSTRUCTOR
+	
+	public Chocolate(Integer id, String name, String type, Float cocoa_perc, String flavour, Integer unit,
+			String shape) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.type = type;
+		this.cocoa_perc = cocoa_perc;
+		this.flavour = flavour;
+		this.unit = unit;
+		this.shape = shape;
+	}
+	//GETTERS Y SETTERS
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public Float getCocoa_perc() {
+		return cocoa_perc;
+	}
+	public void setCocoa_perc(Float cocoa_perc) {
+		this.cocoa_perc = cocoa_perc;
+	}
+	public String getFlavour() {
+		return flavour;
+	}
+	public void setFlavour(String flavour) {
+		this.flavour = flavour;
+	}
+	public Integer getUnit() {
+		return unit;
+	}
+	public void setUnit(Integer unit) {
+		this.unit = unit;
+	}
+	public String getShape() {
+		return shape;
+	}
+	public void setShape(String shape) {
+		this.shape = shape;
+	}
+	@Override
+	public String toString() {
+		return "Chocolate [id=" + id + ", name=" + name + ", type=" + type + ", cocoa_perc=" + cocoa_perc + ", flavour="
+				+ flavour + ", unit=" + unit + ", shape=" + shape + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
 			return true;
-		}
-		
-		
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Chocolate other = (Chocolate) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
 }
+
+
+
