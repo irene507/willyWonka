6e5a4a359a54add07f4aaba097a6a4d4ab487277
Propietario <Propietario@DESKTOP-U9AVVA6>
2020-05-08 19:59:15 +0200

    cambios client y warehouse

--- src/db/interfaces/ClientManager.java
+++ src/db/interfaces/ClientManager.java
@@ -1,14 +1,13 @@
 package db.interfaces;
-import java.sql.Date;
+
 import java.util.List;
 
-import pojos.Chocolate;
 import pojos.Client;
 
 public interface ClientManager{
 	public void addClient(Client client);
 	public void delete(int clientId);
-	public void select(Client client);
+
 	public void update(Client client);
 	public List<Client> searchByName(String name);
 	public List<Client> searchByEmail(String email);
